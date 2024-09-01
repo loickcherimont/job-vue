162357836af16be44a076b2c6717ebb73f9e3416
loickcherimont <loickcherimont@gmail.com>
2024-09-01 23:46:50 +0200

    Process paths that don't exist

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- This view is returned if user enters another path than: 
+        '/', '/jobs', '/login' 
+    -->
+    <div class='not-found text-slate-200 grid grid-rows-2 grid-cols-3 place-items-center h-full'>
+        <h1 class='text-slate-200 text-5xl font-bold'>Error <span class='text-emerald-300'>404</span></h1>
+        <p class='text-slate-400 text-3xl'>Sorry, this page doesn't exist!</p>
+    </div>
+</template>
