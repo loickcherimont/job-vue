bb1f5a5da186de61927b76dc200252cb0d0efce5
loickcherimont <loickcherimont@gmail.com>
2024-09-01 23:45:29 +0200

    Delete all unused comments and keep a memory help for next upkeep

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,19 +1,6 @@
 <template>
-    <!-- <label :for="content.toLowerCase()">{{ content }}</label>
-    <input type='text' :name='content' :id='content.toLowerCase()' v-model="data">
-    <br/> -->
+    <!-- use me to factorize <label> and <input> -->
+    <!-- You'll need this link too : https://vuejs.org/guide/components/v-model.html#component-v-model -->
 </template>
 <script setup>
-// import { computed } from 'vue';
-
-
-// const props = defineProps({
-//     id: Number,
-//     content: String,
-//     data: Object
-// })
-
-// const inputType = computed(() => {
-
-// })
 </script>
